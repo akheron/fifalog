ee2d9e8a7a1c9ad42979d47af5cc69074a726f25
Petri Lehtinen <petri.lehtinen@reaktor.com>
2019-08-29 13:34:45 +0300

    Fix an exception when matches are deleted

--- frontend/components/MatchList.tsx
+++ frontend/components/MatchList.tsx
@@ -2,23 +2,21 @@ import * as React from 'react'
 import { Atom, F } from '@grammarly/focal'
 import { deleteMatch } from '../mutations'
 import { State } from '../state'
-import { indexLens } from '../utils'
+import { indexLens, NonNullAtom } from '../utils'
 import MatchRow from './MatchRow'
 
 const MatchList = (props: { rows: Atom<State.MatchRow[]> }) => (
   <F.div>
-    {props.rows.view(
-      rows =>
-        rows &&
-        rows.map((row, i) => (
-          <MatchRow
-            key={row.match.id}
-            row={props.rows.lens(indexLens<State.MatchRow>(i))}
-            onRemove={() =>
-              confirm('Really?') && deleteMatch(props.rows, row.match.id)
-            }
-          />
-        ))
+    {props.rows.view(rows =>
+      rows.map((row, i) => (
+        <MatchRow
+          key={row.match.id}
+          row={new NonNullAtom(props.rows.lens(indexLens<State.MatchRow>(i)))}
+          onRemove={() =>
+            confirm('Really?') && deleteMatch(props.rows, row.match.id)
+          }
+        />
+      ))
     )}
   </F.div>
 )

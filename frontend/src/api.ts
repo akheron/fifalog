1a7d5b146e849ea06f922312d9c6a4ae2f8f4c6c
Petri Lehtinen <petri@digip.org>
2020-10-17 20:35:56 +0300

    Add parallel effects, use for initial data fetching

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,42 +2,33 @@ import { Match, User, Stats } from '../../common/types'
 import { MatchResultBody } from '../../common/types'
 import * as Effect from './effect'
 
-export const login = Effect.fromPromise(async (credentials: {
-  username: string
-  password: string
-}) => {
-  const response = await fetch('/auth/login', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(credentials),
-  })
-  return response.ok
-})
+export const login = Effect.fromPromise(
+  async (credentials: {
+    username: string
+    password: string
+  }): Promise<boolean> => {
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(credentials),
+    })
+    return response.ok
+  }
+)
 
-export const logout = Effect.fromPromise(async () => {
-  await fetch('/auth/logout')
-})
+export const logout = Effect.fromPromise(
+  async (): Promise<void> => {
+    await fetch('/auth/logout')
+  }
+)
 
-export async function initialData(): Promise<{
-  users: User[]
-  matches: Match[]
-  stats: Stats[]
-}> {
-  const [users_, matches, stats_] = await Promise.all([
-    users(),
-    latestMatches(),
-    stats(),
-  ])
-  return { users: users_, matches, stats: stats_ }
-}
+export const users = Effect.fromPromise(
+  async (): Promise<User[]> => (await fetch('/api/users')).json()
+)
 
-export async function users(): Promise<User[]> {
-  return (await fetch('/api/users')).json()
-}
-
-export async function latestMatches(): Promise<Match[]> {
-  return (await fetch('/api/matches')).json()
-}
+export const latestMatches_ = async (): Promise<Match[]> =>
+  (await fetch('/api/matches')).json()
+export const latestMatches = Effect.fromPromise(latestMatches_)
 
 export async function deleteMatch(id: number): Promise<boolean> {
   return (await fetch(`/api/matches/${id}`, { method: 'DELETE' })).ok
@@ -74,6 +65,20 @@ export async function finishMatch(
   ).json()
 }
 
-export async function stats(): Promise<Stats[]> {
-  return (await fetch('/api/stats')).json()
-}
+export const stats_ = async (): Promise<Stats[]> =>
+  (await fetch('/api/stats')).json()
+export const stats = Effect.fromPromise(stats_)
+
+const void3: (arg: void) => [void, void, void] = () => [
+  undefined,
+  undefined,
+  undefined,
+]
+
+export const initialData = Effect.mapArg(
+  Effect.map(
+    Effect.parallel(users, latestMatches, stats),
+    ([users, matches, stats]) => ({ users, matches, stats })
+  ),
+  void3
+)
--- frontend/src/effect.ts
+++ frontend/src/effect.ts
@@ -1,5 +1,6 @@
 import * as B from 'baconjs'
 import { Atom, onUnmount } from 'harmaja'
+import * as A from './atom-utils'
 
 export type EffectState<E, T> = NotStarted | Pending | Success<T> | Error<E>
 
@@ -21,6 +22,20 @@ export const error = <E, T>(error: E): EffectState<E, T> => ({
   error,
 })
 
+export const isNotStarted_ = (
+  state: EffectState<any, any>
+): state is NotStarted => state.kind === 'NotStarted'
+
+export const isPending_ = (state: EffectState<any, any>): state is Pending =>
+  state.kind === 'Pending'
+
+export const isSuccess_ = <T>(
+  state: EffectState<any, T>
+): state is Success<T> => state.kind === 'Success'
+
+export const isError_ = <E>(state: EffectState<E, any>): state is Error<E> =>
+  state.kind === 'Error'
+
 export interface Effect<A, E, T> {
   run(arg: A): void
   state: B.Property<EffectState<E, T>>
@@ -37,6 +52,24 @@ export function state<E, T>(
 ): B.Property<EffectState<E, T>> {
   return effect.state
 }
+
+export const match = <A, E, T, O>(
+  effect: Effect<A, E, T>,
+  mapPending: () => O,
+  mapSuccess: (value: B.Property<T>) => O,
+  mapError: (error: B.Property<E>) => O
+) =>
+  A.match(
+    A.editAtom(effect.state), // TODO: Fix A.match to work on properties
+
+    isSuccess_,
+    successAtom => mapSuccess(successAtom.view('value')),
+
+    isError_,
+    errorAtom => mapError(errorAtom.view('error')),
+
+    mapPending
+  )
 
 export function isSuccess(effect: Effect<any, any, any>): B.Property<boolean> {
   return effect.state.map(s => s.kind === 'Success')
@@ -139,6 +172,37 @@ function makeEffectConstructor<A, E, T>(
   }
 }
 
+export function map<A, E, T, U>(
+  ctor: EffectConstructor<A, E, T>,
+  fn: (value: T) => U
+): EffectConstructor<A, E, U> {
+  return () => {
+    const effect = ctor()
+    return {
+      run: effect.run,
+      state: effect.state.map(state => {
+        if (state.kind === 'Success') {
+          return success(fn(state.value))
+        }
+        return state
+      }),
+    }
+  }
+}
+
+export function mapArg<A, B, E, T>(
+  ctor: EffectConstructor<A, E, T>,
+  fn: (arg: B) => A
+): EffectConstructor<B, E, T> {
+  return () => {
+    const effect = ctor()
+    return {
+      run: (arg: B) => effect.run(fn(arg)),
+      state: effect.state,
+    }
+  }
+}
+
 export function fromPromise<A, T>(
   fn: () => Promise<T>
 ): () => Effect<void, void, T>
@@ -157,5 +221,93 @@ export function fromPromise<A, T>(
     )
   })
 }
+
+function par<A1, E1, T1, A2, E2, T2>(
+  first: EffectConstructor<A1, E1, T1>,
+  second: EffectConstructor<A2, E2, T2>
+): EffectConstructor<readonly [A1, A2], E1 | E2, readonly [T1, T2]> {
+  const e1 = first()
+  const e2 = second()
+  return makeEffectConstructor((bus, [arg1, arg2]) => {
+    let done = false
+    e1.run(arg1)
+    e2.run(arg2)
+    bus.push(pending())
+    bus.plug(
+      B.combine(
+        e1.state,
+        e2.state,
+        (s1, s2) => [s1, s2] as const
+      ).flatMapLatest(([s1, s2]):
+        | EffectState<E1 | E2, [T1, T2]>
+        | B.EventStream<EffectState<E1 | E2, [T1, T2]>> => {
+        if (done) return B.never<EffectState<E1 | E2, [T1, T2]>>()
+        done = true
+        if (s1.kind === 'Error') return s1
+        if (s2.kind === 'Error') return s2
+        if (s1.kind === 'Success' && s2.kind === 'Success')
+          return success([s1.value, s2.value])
+        done = false
+        return B.never<EffectState<E1 | E2, [T1, T2]>>()
+      })
+    )
+  })
+}
+
+export function parallel<A1, E1, T1, A2, E2, T2>(
+  first: EffectConstructor<A1, E1, T1>,
+  second: EffectConstructor<A2, E2, T2>
+): EffectConstructor<[A1, A2], E1 | E2, [T1, T2]>
+export function parallel<A1, E1, T1, A2, E2, T2, A3, E3, T3>(
+  first: EffectConstructor<A1, E1, T1>,
+  second: EffectConstructor<A2, E2, T2>,
+  third: EffectConstructor<A3, E3, T3>
+): EffectConstructor<[A1, A2, A3], E1 | E2 | E3, [T1, T2, T3]>
+export function parallel<A1, E1, T1, A2, E2, T2, A3, E3, T3, A4, E4, T4>(
+  first: EffectConstructor<A1, E1, T1>,
+  second: EffectConstructor<A2, E2, T2>,
+  third: EffectConstructor<A3, E3, T3>,
+  fourth: EffectConstructor<A4, E4, T4>
+): EffectConstructor<[A1, A2, A3, A4], E1 | E2 | E3 | E4, [T1, T2, T3, T4]>
+export function parallel<A1, E1, T1, A2, E2, T2, A3, E3, T3, A4, E4, T4>(
+  first: EffectConstructor<A1, E1, T1>,
+  second: EffectConstructor<A2, E2, T2>,
+  third?: EffectConstructor<A3, E3, T3>,
+  fourth?: EffectConstructor<A4, E4, T4>
+):
+  | EffectConstructor<readonly [A1, A2], E1 | E2, readonly [T1, T2]>
+  | EffectConstructor<
+      readonly [A1, A2, A3],
+      E1 | E2 | E3,
+      readonly [T1, T2, T3]
+    >
+  | EffectConstructor<
+      readonly [A1, A2, A3, A4],
+      E1 | E2 | E3 | E4,
+      readonly [T1, T2, T3, T4]
+    > {
+  switch (arguments.length) {
+    case 2:
+      return par(first, second)
+    case 3:
+      return mapArg(
+        map(
+          par(par(first, second), third!),
+          ([[t1, t2], t3]) => [t1, t2, t3] as const
+        ),
+        ([a1, a2, a3]: [A1, A2, A3]) => [[a1, a2], a3] as const
+      )
+    default:
+      return mapArg(
+        map(
+          par(par(par(first, second), third!), fourth!),
+          ([[[t1, t2], t3], t4]) => [t1, t2, t3, t4] as const
+        ),
+        ([a1, a2, a3, a4]: [A1, A2, A3, A4]) => [[[a1, a2], a3], a4] as const
+      )
+  }
+}
+
+//
 
 const identity = <T>(x: T): T => x

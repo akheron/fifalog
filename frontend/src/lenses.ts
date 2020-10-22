a3e3dd2316fc79bbf702c9549d41b2067868562b
Petri Lehtinen <petri@digip.org>
2020-10-22 17:36:19 +0300

    Remove the unused unsafe find lens

--- frontend/src/lenses.ts
+++ frontend/src/lenses.ts
@@ -1,16 +1,5 @@
 import { Lens } from 'harmaja'
 
-// Unsafe: pred must return true for one of the elements
-export function find<T>(pred: (x: T) => boolean): Lens<T[], T> {
-  return {
-    get: root => root.find(pred)!,
-    set: (root, value) => {
-      const index = root.findIndex(pred)!
-      return [...root.slice(0, index), value, ...root.slice(index + 1)]
-    },
-  }
-}
-
 export const pick = <T, K extends keyof T>(keys: K[]): Lens<T, Pick<T, K>> => ({
   get(root) {
     const result: Pick<T, K> = {} as any

076d707f87c04c7ff54fdd585f71a486098fdf28
Petri Lehtinen <petri@digip.org>
2020-11-07 12:28:27 +0200

    Refactor dbClient

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -23,122 +23,111 @@ type TotalStats = {
   goals: number
 }
 
-export type DBClient = {
-  users(): Promise<User[]>
-  leagues(): Promise<League[]>
-  match(id: number): Promise<Match | null> // null -> no such match
-  deleteMatch(id: number): Promise<boolean> // true -> was actually removed
-  finishMatch(id: number, result: MatchResultBody): Promise<Match | null> // null -> no such match
-  latestMatches(count: number): Promise<Match[]>
-  createMatch(matchData: {
+export type DBClient = ReturnType<typeof dbClient>
+
+const dbClient = (client: pg.ClientBase) => ({
+  async users(): Promise<User[]> {
+    return sql.users(client)
+  },
+
+  async leagues(): Promise<League[]> {
+    const rows = await sql.leagues(client, { matchPairsToExclude: 5 })
+    return R.groupWith((a, b) => a.league_name === b.league_name, rows).map(
+      rows => ({
+        id: rows[0].league_id,
+        name: rows[0].league_name,
+        teams: rows.map(row => ({
+          id: row.team_id,
+          name: row.team_name,
+        })),
+      })
+    )
+  },
+
+  /**
+   * null -> no such match
+   */
+  async match(id: number): Promise<Match | null> {
+    const row = await sql.match(client, { matchId: id })
+    return row && matchFromRow(row)
+  },
+
+  /**
+   *  true -> was actually removed
+   */
+  async deleteMatch(id: number): Promise<boolean> {
+    return (await sql.deleteMatch(client, { matchId: id })) === 1
+  },
+
+  /**
+   * null -> no such match
+   */
+  async finishMatch(
+    id: number,
+    result: MatchResultBody
+  ): Promise<Match | null> {
+    const rowCount = await sql.finishMatch(client, {
+      matchId: id,
+      homeScore: result.homeScore,
+      awayScore: result.awayScore,
+      finishedType: result.finishedType.kind,
+      homePenaltyGoals: MatchResultBody.isPenalties(result.finishedType)
+        ? result.finishedType.homeGoals
+        : null,
+      awayPenaltyGoals: MatchResultBody.isPenalties(result.finishedType)
+        ? result.finishedType.awayGoals
+        : null,
+    })
+
+    if (rowCount == 0) return null
+    return await this.match(id)
+  },
+
+  async latestMatches(count: number): Promise<Match[]> {
+    const rows = await sql.latestMatches(client, { limit: count })
+    return rows.map(matchFromRow)
+  },
+
+  async createMatch(params: {
     leagueId: number
     homeId: number
     awayId: number
     homeUserId: number
     awayUserId: number
-  }): Promise<Match | null>
-  userStats(limit: number | null): Promise<MonthUserStats[]>
-  totalStats(limit: number | null): Promise<TotalStats[]>
-}
+  }): Promise<Match | null> {
+    let result = await onIntegrityError(null, sql.createMatch(client, params))
+    if (result == null) return null
+    return await this.match(result.id)
+  },
 
-const dbClient = (client: pg.ClientBase): DBClient => {
-  const dbClient: DBClient = {
-    async users() {
-      return sql.users(client)
-    },
+  async userStats(limit: number | null): Promise<MonthUserStats[]> {
+    const rows = await sql.userStats(client, {
+      limit: limit === null ? 0 : limit,
+    })
+    return rows.map(r => ({
+      month: r.month,
+      user: {
+        id: r.user_id,
+        name: r.user_name,
+      },
+      wins: r.win_count,
+      overTimeWins: r.overtime_win_count,
+      goalsFor: r.goals_for,
+    }))
+  },
 
-    async leagues() {
-      const rows = await sql.leagues(client, { matchPairsToExclude: 5 })
-      return R.groupWith((a, b) => a.league_name === b.league_name, rows).map(
-        rows => ({
-          id: rows[0].league_id,
-          name: rows[0].league_name,
-          teams: rows.map(row => ({
-            id: row.team_id,
-            name: row.team_name,
-          })),
-        })
-      )
-    },
-
-    async match(id) {
-      const row = await sql.match(client, { matchId: id })
-      return row && matchFromRow(row)
-    },
-
-    async deleteMatch(id: number) {
-      return (await sql.deleteMatch(client, { matchId: id })) === 1
-    },
-
-    async finishMatch(id: number, result: MatchResultBody) {
-      const rowCount = await sql.finishMatch(client, {
-        matchId: id,
-        homeScore: result.homeScore,
-        awayScore: result.awayScore,
-        finishedType: result.finishedType.kind,
-        homePenaltyGoals: MatchResultBody.isPenalties(result.finishedType)
-          ? result.finishedType.homeGoals
-          : null,
-        awayPenaltyGoals: MatchResultBody.isPenalties(result.finishedType)
-          ? result.finishedType.awayGoals
-          : null,
-      })
-
-      if (rowCount == 0) return null
-      return await this.match(id)
-    },
-
-    async latestMatches(count) {
-      const rows = await sql.latestMatches(client, { limit: count })
-      return rows.map(matchFromRow)
-    },
-
-    async createMatch({ leagueId, homeId, awayId, homeUserId, awayUserId }) {
-      let result = await onIntegrityError(
-        null,
-        sql.createMatch(client, {
-          leagueId,
-          homeId,
-          awayId,
-          homeUserId,
-          awayUserId,
-        })
-      )
-      if (result == null) return null
-      return await this.match(result.id)
-    },
-
-    async userStats(limit: number | null) {
-      const rows = await sql.userStats(client, {
-        limit: limit === null ? 0 : limit,
-      })
-      return rows.map(r => ({
-        month: r.month,
-        user: {
-          id: r.user_id,
-          name: r.user_name,
-        },
-        wins: r.win_count,
-        overTimeWins: r.overtime_win_count,
-        goalsFor: r.goals_for,
-      }))
-    },
-
-    async totalStats(limit: number | null) {
-      const rows = await sql.totalStats(client, {
-        limit: limit === null ? 0 : limit,
-      })
-      return rows.map(r => ({
-        month: r.month,
-        matches: r.match_count,
-        ties: r.tie_count,
-        goals: r.goal_count,
-      }))
-    },
-  }
-
-  return dbClient
-}
+  async totalStats(limit: number | null): Promise<TotalStats[]> {
+    const rows = await sql.totalStats(client, {
+      limit: limit === null ? 0 : limit,
+    })
+    return rows.map(r => ({
+      month: r.month,
+      matches: r.match_count,
+      ties: r.tie_count,
+      goals: r.goal_count,
+    }))
+  },
+})
 
 const forceSslOptions = {
   rejectUnauthorized: false,

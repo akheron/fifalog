d69aff7eedca25a7a2347aa9b7c95bc632c07db3
Petri Lehtinen <petri@digip.org>
2022-07-23 11:25:46 +0300

    Hide older stats by default

--- frontend/src/components/Stats.tsx
+++ frontend/src/components/Stats.tsx
@@ -3,25 +3,47 @@ import React from 'react'
 import { MonthStats, useStatsQuery } from '../stats/statsApi'
 
 import * as styles from './Stats.module.css'
+import { useBooleanState } from '../utils/state'
+import { VGap } from './whitespace'
 
 export default React.memo(function Stats() {
   const { data, isLoading } = useStatsQuery()
+  const expand = useBooleanState(false)
+
   if (isLoading) return <div>Loading...</div>
+  if (!data) return <div>Error loading stats</div>
+
+  const first5 = data.slice(0, 5)
+  const rest = data.slice(5)
+
   return (
-    <table className={styles.stats}>
-      <thead>
-        <tr>
-          <th></th>
-          <th></th>
-          <th>W</th>
-          <th></th>
-          <th>G</th>
-        </tr>
-      </thead>
-      {data?.map((month) => (
-        <Month key={month.month} stats={month} />
-      ))}
-    </table>
+    <>
+      <table className={styles.stats}>
+        <thead>
+          <tr>
+            <th></th>
+            <th></th>
+            <th>W</th>
+            <th></th>
+            <th>G</th>
+          </tr>
+        </thead>
+        {first5.map((month) => (
+          <Month key={month.month} stats={month} />
+        ))}
+        {expand.value
+          ? rest.map((month) => <Month key={month.month} stats={month} />)
+          : null}
+      </table>
+      <VGap />
+      {rest.length > 0 ? (
+        <div>
+          <button onClick={expand.toggle}>
+            show {rest.length} {expand.value ? 'less' : 'more'}
+          </button>
+        </div>
+      ) : null}
+    </>
   )
 })
 
